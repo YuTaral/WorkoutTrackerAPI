759146d1dab84de4a05723b7afd080717487f466
yutaral99 <yutaral99@gmail.com>
2024-09-22 16:42:57 +0300

    Removes unused method from IWourkoutService interface

--- FitnessAppAPI/Data/Services/Workouts/IWorkoutService.cs
+++ FitnessAppAPI/Data/Services/Workouts/IWorkoutService.cs
@@ -9,7 +9,6 @@ namespace FitnessAppAPI.Data.Services.Workouts
     {
         public WorkoutModel? AddWorkout(WorkoutModel data, string userId);
         public WorkoutModel? GetWorkout(long id);
-        public WorkoutModel? GetWorkoutFromExercise(long exerciseId);
         public WorkoutModel? GetTodayWorkout(string userId);
         public bool AddExercise(ExerciseModel set, long workoutId);
         public bool UpdateExercise(ExerciseModel exercise, long workoutId);
